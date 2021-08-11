042029f2efe31d66f8c7af3ded32871a40af5775
Leonardo Zamarchi <leonardozamarchi@gmail.com>
2021-08-11 15:07:37 -0300

    Otimização de código

--- js/form.js
+++ js/form.js
@@ -4,18 +4,17 @@ botaoAdicionar.addEventListener('click', function(event){
     event.preventDefault();
     
     var form = document.querySelector('#form-adiciona')
-
     var paciente = obtemPacienteFormulario(form)
-
-    var pacienteTr = montaTr(paciente)
-
-    
+    var pacienteTr = montaTr(paciente)    
     var tabela = document.querySelector('#tabela-pacientes')
 
     tabela.appendChild(pacienteTr)
-})
-function obtemPacienteFormulario(form) {
 
+    form.reset()
+})
+
+
+function obtemPacienteFormulario(form) {
     var paciente = {
         nome: form.nome.value,
         peso: form.peso.value,
@@ -28,23 +27,21 @@ function obtemPacienteFormulario(form) {
 
 function montaTr(paciente) {
     var pacienteTr = document.createElement('tr')
+    pacienteTr.classList.add('paciente')
 
-    var nomeTd = document.createElement('td')
-    var pesoTd = document.createElement('td')
-    var alturaTd = document.createElement('td')
-    var gorduraTd = document.createElement('td')
-    var imcTd = document.createElement('td')
-
-    nomeTd.textContent = paciente.nome
-    pesoTd.textContent = paciente.peso
-    alturaTd.textContent = paciente.altura
-    gorduraTd.textContent = paciente.gordura
-    imcTd.textContent = paciente.imc
-
-    pacienteTr.appendChild(nomeTd)
-    pacienteTr.appendChild(pesoTd)
-    pacienteTr.appendChild(alturaTd)
-    pacienteTr.appendChild(gorduraTd)
-    pacienteTr.appendChild(imcTd) 
+    pacienteTr.appendChild(montaTd(paciente.nome,'info-nome'))
+    pacienteTr.appendChild(montaTd(paciente.peso,'info-peso'))
+    pacienteTr.appendChild(montaTd(paciente.altura,'info-altura'))
+    pacienteTr.appendChild(montaTd(paciente.gordura,'info-gordura'))
+    pacienteTr.appendChild(montaTd(paciente.imc,'info-imc')) 
+    
     return pacienteTr   
+}
+
+function montaTd(dado, classe){
+    var td = document.createElement('td')
+    td.textContent = dado
+    td.classList.add(classe)
+
+    return td
 }
